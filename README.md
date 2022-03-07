11a1ec8704d51a1e1367e1e8b3d10fee396c81d4
Maya Tillman <mayatillman17@gmail.com>
2022-03-06 20:45:56 -0600

    notes have successfully been added message

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Note-Taker
+# 10 Note-Taker
+
+## Description
+
+This applciation accepts user input and will display previous and current notes. The application relies heavily on front end development and Express.js for back end development to save and retrieve note data from a JSON file.
+
+## Screenshot
+
+![The Note-Taker webpage includes a text area that accepts user input and will display previous and current notes on the side.](./assets/images/)
+
+## Links to Application
+
+- Here is the repo : [mayaatil/repo](https://github.com/mayaatil/note-taker)
+- Here is the pages: [mayaatil/pages](https://mayaatil.github.io/note-taker)
---
+++ server.js
@@ -0,0 +1,73 @@
+//require
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const util = require("util");
+const { v4: uuidv4 } = require("uuid");
+
+//initialize port and express
+const app = express();
+const PORT = 3001;
+
+//sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//get fetch for intial homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/assets/index.html"));
+});
+
+//get fetch for the notes page
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/asets/notes.hmtl"));
+});
+
+const readFromFile = util.promisify(fs.readFile);
+
+//writeFile
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written`)
+  );
+
+const readAndAppend = (content, file) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+//use get to receive notes as a string
+app.get("/api/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+//post request
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      title_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note successfully added!`);
+  } else {
+    res.error("Error in adding note");
+  }
+});
+
+//listener
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
